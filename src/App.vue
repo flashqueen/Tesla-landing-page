6c494c078a1e745525a1d31612cf49398468175f
Danilo Oliveira <dohkoweeds74@gmail.com>
2023-05-21 22:57:40 -0300

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,28 @@
 <template>
-  <h1 class="text-3xl font-bold underline text-red-500">
-    Hello world!
-  </h1>
+  <header class="navbar">
+    <!-- Logo -->
+    <img src="../public/images/tesla.svg" alt="tesla logo" class="h-6">
+    <!-- Nav Links -->
+    <div>
+      <a href="#modelS" class="nav-link">Model S</a>
+      <a href="#model3" class="nav-link">Model 3</a>
+      <a href="#modelX" class="nav-link">Model X</a>
+      <a href="#modelY" class="nav-link">Model Y</a>
+      <a href="#solarRoof" class="nav-link">Solar Roof</a>
+      <a href="#solarPanels" class="nav-link">Solar Panels</a>
+    </div>
+    <!-- ext links -->
+    <div>
+      <a href="#shop" class="nav-link">Shop</a>
+      <a href="#account" class="nav-link">Account</a>
+      <a href="#menu" class="nav-link">Menu</a>
+    </div>
+  </header>
+
+
+
+
+
+
 </template>
 
